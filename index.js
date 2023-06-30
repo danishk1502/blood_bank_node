fe9b8143e1152f91e27a525a8cae15f65c8a6fc7
Danish <kdanishwins@gmail.com>
2023-06-30 18:49:54 +0530

    mysql connection commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 5645;
+const mysql =require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"blood_bank_db"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected");
+  const sql = "INSERT INTO user (Name, Email) VALUES ('Danish', 'kdanishwins')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+});
+
+
 
 app.use("/", (req,res)=>{
     res.send("hii this is first port")
