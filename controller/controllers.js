4d470cb9882fe88558f51670e6823cfde707e1b9
Danish <kdanishwins@gmail.com>
2023-07-13 16:23:04 +0530

    setup status as per role

--- controller/controllers.js
+++ controller/controllers.js
@@ -3,50 +3,50 @@ const bcrypt = require('bcrypt');
 
 //user Creation controllers 
 
-exports.registration =  (async(req,res)=>{
+exports.registration = (async (req, res) => {
     const name = req.body.name;
     const lname = req.body.lname;
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
     const username = req.body.username
-    
-    const allData =pool.query(`SELECT * FROM users WHERE username="${username}"`  , (err, result)=>{
+
+    const allData = pool.query(`SELECT * FROM users WHERE username="${username}"`, (err, result) => {
         if (err) throw err;
-        if(result.length == 0){
-            pool.query(`INSERT INTO users (name, lname, password, username) VALUES ('${name}', '${lname}', '${password}', '${username}')`, (err)=>{
-                                    if (err) throw err;
-                                })
+        if (result.length == 0) {
+            pool.query(`INSERT INTO users (name, lname, password, username) VALUES ('${name}', '${lname}', '${password}', '${username}')`, (err) => {
+                if (err) throw err;
+            })
             res.send("you are registered")
         }
-        else{
+        else {
             res.send("user exists");
         }
-})
+    })
 })
 
 
 
 
 // log in check
-exports.login = ((req, res)=>{
-    pool.query(`SELECT * FROM users WHERE username="${req.body.username}"` ,(err, result)=>{
-        if(err) throw err;
-       if(result.length == 0){
+exports.login = ((req, res) => {
+    pool.query(`SELECT * FROM users WHERE username="${req.body.username}"`, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
             res.send("user not exist")
         }
-        else{
-            for(let i=0; i<result.length; i++){
+        else {
+            for (let i = 0; i < result.length; i++) {
                 bcrypt.compare(req.body.password, result[i].password, (err, bcryptResult) => {
                     if (err) throw err;
-                    if(!bcryptResult){
+                    if (!bcryptResult) {
                         res.send("wrong password")
                     }
-                    else{
+                    else {
                         res.send("you are logged in...");
                     }
-            })
+                })
             }
-         
+
 
         }
     })
@@ -60,8 +60,8 @@ exports.login = ((req, res)=>{
 
 
 // for data get controller
-exports.dataShow = ((req,res)=>{
-    const data =pool.query(`SELECT * from users`, (err, result)=>{
+exports.dataShow = ((req, res) => {
+    const data = pool.query(`SELECT * from users`, (err, result) => {
         if (err) throw err;
         res.send(result);
     })
--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const app = express();
 const router = require('./routes/routers');
 
 app.use(express.json());
-
 app.use('/', router);
 
 
-
-
-app.listen(process.env.APP_PORT, ()=>{
-    console.log("app listen at port :"+ process.env.APP_PORT);
+app.listen(process.env.APP_PORT, () => {
+    console.log("app listen at port :" + process.env.APP_PORT);
 })
