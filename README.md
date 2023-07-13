f2f9cc1a8bb36fe2a9768f5b40538b1860b53a31
Danish <kdanishwins@gmail.com>
2023-07-13 18:34:36 +0530

    changing port and adding email verification for registration

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # blood_bank_node
+
+# Database_fields_for_user_table 
+
+user table contain data of User(Requester and Donor), Blood Bank and Superuser
+
+id   :  Primary Key (Auto)
+name : varchar
+lname : varchar
+username ; varchar
+password : varchar (bcript)
+email : varchar
+state : varchar
+distt : varchar
+user_status : Enum
+is_deleted : Enum
+role : Enum
+number_of_donations : Number
+created_by : Varchar
+updated_by : varchar
+created_at : Time
+updated_at : Time
--- controller/controllers.js
+++ controller/controllers.js
@@ -8,21 +8,65 @@ exports.registration = (async (req, res) => {
     const lname = req.body.lname;
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
-    const username = req.body.username
-
-    const allData = pool.query(`SELECT * FROM users WHERE username="${username}"`, (err, result) => {
-        if (err) throw err;
-        if (result.length == 0) {
-            pool.query(`INSERT INTO users (name, lname, password, username) VALUES ('${name}', '${lname}', '${password}', '${username}')`, (err) => {
-                if (err) throw err;
-            })
-            res.send("you are registered")
-        }
-        else {
-            res.send("user exists");
-        }
-    })
+    const username = req.body.username;
+    const email = req.body.email;
+    const role = req.body.role;
+    const state = req.body.state;
+    const distt = req.body.distt;
+    const is_deleted = false;
+    const created_by = req.body.username;
+    const updated_by = req.body.username;;
+    let user_status = null;
+    if (role === "user") {
+        user_status = 'Active'
+        const allData = pool.query(`SELECT * FROM users WHERE username="${username}" || email="${email}"`, (err, result) => {
+            if (err) throw err;
+            if (result.length == 0) {
+                pool.query(`INSERT INTO users (name, lname, password, username, email, state, distt, user_status, is_deleted, role, created_by, updated_by ) VALUES('${name}', '${lname}', '${password}', '${username}','${email}', '${state}', '${distt}', '${user_status}','${is_deleted}', '${role}', '${created_by}', '${updated_by}')`, (err) => {
+                    if (err) throw err;
+                })
+                res.send("you are registered")
+            }
+            else {
+                res.send("user exists on this email or username");
+            }
+        })
+    }
+    else if (role == "blood_bank") {
+        user_status = 'Deactivate'
+        const allData = pool.query(`SELECT * FROM users WHERE username="${username}"`, (err, result) => {
+            if (err) throw err;
+            if (result.length == 0) {
+                pool.query(`INSERT INTO users (name, lname, password, username, email, state, distt, user_status, is_deleted, role, created_by, updated_by ) VALUES('${name}', '${lname}', '${password}', '${username}','${email}', '${state}', '${distt}', '${user_status}','${is_deleted}', '${role}', '${created_by}', '${updated_by}')`, (err) => {
+                    if (err) throw err;
+                })
+                res.send("you are registered")
+            }
+            else {
+                res.send("user exists");
+            }
+        })
+    }
+    else if (role == "superuser") {
+        user_status = 'Activate'
+        const allData = pool.query(`SELECT * FROM users WHERE username="${username}"`, (err, result) => {
+            if (err) throw err;
+            if (result.length == 0) {
+                pool.query(`INSERT INTO users (name, lname, password, username, email, state, distt, user_status, is_deleted, role, created_by, updated_by ) VALUES('${name}', '${lname}', '${password}', '${username}','${email}', '${state}', '${distt}', '${user_status}','${is_deleted}', '${role}', '${created_by}', '${updated_by}')`, (err) => {
+                    if (err) throw err;
+                })
+                res.send("you are registered")
+            }
+            else {
+                res.send("user exists");
+            }
+        })
+    }
+    else {
+        res.send("Enter a valid role");
+    }
 })
+
 
 
 
@@ -36,26 +80,24 @@ exports.login = ((req, res) => {
         }
         else {
             for (let i = 0; i < result.length; i++) {
-                bcrypt.compare(req.body.password, result[i].password, (err, bcryptResult) => {
-                    if (err) throw err;
-                    if (!bcryptResult) {
-                        res.send("wrong password")
-                    }
-                    else {
-                        res.send("you are logged in...");
-                    }
-                })
+                if (result[i].user_status == "deactivate") {
+                    res.send("Sorry Your request is under progress")
+                }
+                else {
+                    bcrypt.compare(req.body.password, result[i].password, (err, bcryptResult) => {
+                        if (err) throw err;
+                        if (!bcryptResult) {
+                            res.send("wrong password")
+                        }
+                        else {
+                            res.send("you are logged in...");
+                        }
+                    })
+                }
             }
-
-
         }
     })
 })
-
-
-
-
-
 
 
 
